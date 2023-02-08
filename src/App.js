f07b0d5c14483d7ad36f87e88decb3d60dc23c4e
calvin <ncalvin67@gmail.com>
2023-02-08 10:29:56 +0300

    ✨🎨 Added footer and corrected some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import About from './components/About'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
 import Home from './components/Home'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Home/>
     <About/>
     <Cards/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h2>You can find us on:</h2>
+        <ul>
+            <li>info@hedeshafreighters.com</li>
+            <li>facebook</li>
+            <li>twitter</li>
+            <li>linkedIn</li>
+        </ul>
+        <h3>Or contact us on: +254 735 968 922</h3>
+    </div>
+  )
+}
+
+export default Footer
