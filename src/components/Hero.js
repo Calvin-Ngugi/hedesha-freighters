0482f7586ecc9027a92255e745345fb94c2c3f29
calvin <ncalvin67@gmail.com>
2023-02-08 07:37:40 +0300

    ✨ Added hero commponent

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <h3>Hedesha Freighters</h3>
+        <h1>We Deliver Global Logistics</h1>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Hero from './Hero'
 import Navbar from './Navbar'
 
 const Home = () => {
   return (
     <div className='hero'>
         <Navbar/>
+        <hr/>
+        <Hero/>
     </div>
   )
 }
