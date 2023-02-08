bed7db8a40abdf60df6ad0f872c1b888a4f3ee72
calvin <ncalvin67@gmail.com>
2023-02-08 10:07:48 +0300

    ✨Added about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import About from './components/About'
 import Home from './components/Home'
 
 const App = () => {
   return (
+    <>
     <Home/>
+    <About/>
+    </>
   )
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+      <div className="about">
+        <h1>About Us</h1>
+        <p>
+          Hedesha freighters Ltd was founded in 2004 with a core business of
+          providing Professional and efficient clearing and forwarding services.
+          The company follows a customer driven business approach where
+          internationally acknowledged freighting,warehousing and clearing
+          standards together with customer needs formulate our operational
+          procedure. We have a demanding and diverse client portfolio serviced
+          by a team of dedicated, skilled and highly trained professionals.
+        </p>
+      </div>
+  );
+};
+
+export default About;
